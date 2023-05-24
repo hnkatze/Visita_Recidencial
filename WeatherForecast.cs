f0dacbe906eea6a25e1a3b788e3acf06a9620ac0
hnkatze <henriquezhector1@hotmail.com>
2023-05-23 18:59:52 -0600

    First Upload

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace Visitas_Recidencial;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
